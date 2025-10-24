1a81f6b8ca25dd5adf2b60280b1aefdea98dc929
Tyano4ka14 <Tyano4ka14@DESKTOP-1S10BQB>
2025-10-24 23:24:35 +0500

    а

--- Home learning/Main.cpp
+++ Home learning/Main.cpp
@@ -89,62 +89,164 @@ int *numbers3 {new int[4]{ 1, 2 }};       // массив состоит из чисел 1, 2, 0, 0
 using namespace std;
 
 
-// Функция для увеличения массива
-void expanenda_arr(int*& arr, int& size, int newElement) {
-    // Создаем новый массив на одну ячейку больше
+
+void expanenda_arr(int*& arr, int& size, int new_element,int index) {
+
     int* new_arr = new int[size + 1];
 
-    // Копируем все элементы из старого массива в новый
-    for (int i = 0; i < size; i++) {
+
+    for (int i = 0; i < index - 1; i++) {
         new_arr[i] = arr[i];
     }
+    index += index ;
+    for (int i = index ; i < size; i++) {
+        new_arr[i] = arr[i];
+    }
+    
+    int i = index;
+    new_arr[i] = new_element;
+   
 
-    // Добавляем новый элемент в последнюю ячейку
-    new_arr[size] = newElement;
 
-    // Увеличиваем размер массива
+
+
+
+    new_arr[index] = new_element;
+
+
     size++;
 
-    // Освобождаем память старого массива
-    delete[] arr;
 
-    // Переназначаем указатель на новый массив
+   
+ delete[] arr;
+
     arr = new_arr;
+   
 }
 
 int main() {
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    // Создаем начальный массив из 2 элементов
-    int size = 2;
+int size, element, index, new_element;
+int* arr = new int[size];
+    
+    cout << "Enter size of array: " << endl;
+    cin >> size;
     int* arr = new int[size];
 
-    // Заполняем массив числами
-    arr[0] = 1;
-    arr[1] = 2;
 
-    // Выводим исходный массив
-    cout << "First massive : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter elements: " << endl;
+        cin >> element;
+        element = arr[i];
+    }
+    cout << "Array: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    // Вызываем функцию расширения массива
-    int newElement = 3;
-    expanenda_arr(arr, size, newElement);
+    cout << "Enter index: " << endl;
+    cin >> index;
+    cout << "Enter new element: " << endl;
+    cin >> new_element;
 
-    // Выводим измененный массив
+
+    
+
+
+
+
     cout << " Changed massive: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    // Освобождаем выделенную память
+
+    return 0;
+}
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+
+void expanenda_arr(int*& arr, int& size, int newElement, int index) {
+    if (index < 0 || index > size) {
+        cout << "Error!(index entering) " << endl;
+        return;
+    }
+
+    int* new_arr = new int[size + 1];
+
+    for (int i = 0; i < index; i++) {
+        new_arr[i] = arr[i];
+    }
+
+    new_arr[index] = newElement;
+
+    for (int i = index + 1; i <= size; i++) {
+        new_arr[i] = arr[i - 1];
+    }
+
+    size++;
+
+    delete[] arr;
+    arr = new_arr;
+}
+
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    int size, element, index, newElement;
+    cout << "Enter size: " << endl;
+    cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+    cout << "Enter " << i + 1 << " element: " << endl;
+    cin >> element;
+    arr[i] = element;
+    }
+    
+
+    cout << "First massive : ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Enter index (from " << 0 << " to " << size << "): " << endl;
+    cin >> index;
+
+
+    if (index < 0 || index > size) {
+        cout << "Ошибка: неверный индекс!" << endl;
+        return 1;
+    }
+    
+
+    cout << "Enter new element: " << endl;
+    cin >> newElement;
+    expanenda_arr(arr, size, newElement, index);
+
+  
+    cout << "Changed massive: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+ 
     delete[] arr;
 
     return 0;
-}*/
-//dinamic выше сразу
+}
